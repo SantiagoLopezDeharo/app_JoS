4d47c058a425eddda2227089365f9b5690fe1e5d
santiago lopez de haro <santiagolhgpro@gmail.com>
2024-01-18 20:40:04 -0300

    Hice mi hello world con el framework !!! :D

--- platzi-market/platzi-market/src/main/java/com/platzi/market/PlatziMarketApplication.java
+++ platzi-market/platzi-market/src/main/java/com/platzi/market/PlatziMarketApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PlatziMarketApplication {
+public class PlatziMarketApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(PlatziMarketApplication.class, args);
 	}
 
