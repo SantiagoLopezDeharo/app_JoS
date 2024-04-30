7249e6b5fac82224987356d70969452b49a9fcbf
santiago lopez de haro <santiagolhgpro@gmail.com>
2024-04-30 15:20:26 -0300

    Estuve jugando un poco con Threads

--- java_practice/Hello.java
+++ java_practice/Hello.java
@@ -1,3 +1,50 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.xml.transform.Source;
+
+class sumar
+{
+    private int a = 0;
+    public synchronized void s(int b)
+    {
+        a += b;
+    }
+}
+
+class A extends Thread
+{
+    public void run()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            System.out.println("Lol");
+            try {
+                Thread.sleep(2);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class B extends Thread
+{
+    public void run()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            System.out.println("Lmao");
+            try {
+                Thread.sleep(2);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+}
 
 class Hello
 {
@@ -6,8 +53,23 @@ class Hello
         System.out.println("Hola mundo !");
 
         byte b = 125;
-        int a = (int) b;
+        Integer a = (int) b;
         System.out.println(a);
+        sumar s = new sumar();
+        s.s(3);
+        System.out.println(a);
+        
+        Collection<Integer> nums = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i ++)
+            nums.add(i);
+        for (int i : nums)
+            System.out.println(i);
+
+        A aa = new A();
+        B bb = new B();
+        
+        aa.start();bb.start();
+
     }
 
 }
