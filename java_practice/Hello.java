8a9c91020a37ebf9a9716b8f1947e48435b01649
santiago lopez de haro <santiagolhgpro@gmail.com>
2024-04-30 14:31:59 -0300

    Empece a practicar un poco de Java para aprovechar la introduccion avanzada a Java del curso

---
+++ java_practice/Hello.java
@@ -0,0 +1,13 @@
+
+class Hello
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hola mundo !");
+
+        byte b = 125;
+        int a = (int) b;
+        System.out.println(a);
+    }
+
+}
