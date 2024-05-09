c14b116d9c2b656f36b56fcb59637bcfd65abc83
santiago lopez de haro <santiagolhgpro@gmail.com>
2024-05-09 18:16:49 -0300

    Aprendimos a usar Model And View y con eso queda terminada la seccion de calculadora

--- springbootweb1/src/main/java/com/aprendiendo/springbootweb1/HomeController.java
+++ springbootweb1/src/main/java/com/aprendiendo/springbootweb1/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -15,16 +16,17 @@ public class HomeController {
     }
 
     @RequestMapping("/add")
-    public String add(@RequestParam("num1") int n1,@RequestParam("num2") int n2, Model model)
+    public ModelAndView add(@RequestParam("num1") int n1,@RequestParam("num2") int n2, ModelAndView model)
     {
         //int n1 = Integer.parseInt(req.getParameter("num1"));
         //int n2 = Integer.parseInt(req.getParameter("num2"));
 
         int result = n1 + n2;
         
-        model.addAttribute("resultado", result);
+        model.addObject("resultado", result);
+        model.setViewName("result");
         //session.setAttribute("resultado", result);
 
-        return "result";
+        return model;
     }
 }
