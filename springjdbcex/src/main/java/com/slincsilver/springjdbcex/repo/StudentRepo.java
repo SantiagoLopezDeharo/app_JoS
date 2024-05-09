e66fa6d2e7f20d3ed3cd688ed82ad7b9bb220f67
santiago lopez de haro <santiagolhgpro@gmail.com>
2024-05-09 15:54:23 -0300

    Use un try y catch para prevenir el error de agregar varias veces el mismo estudiante

--- springjdbcex/src/main/java/com/slincsilver/springjdbcex/repo/StudentRepo.java
+++ springjdbcex/src/main/java/com/slincsilver/springjdbcex/repo/StudentRepo.java
@@ -25,11 +25,16 @@ public class StudentRepo {
 
     public void save(Student s)
     {
-        String sql = "insert into student2 (rollno, name, marks) values (?,?,?)";
+        try {
+            String sql = "insert into student2 (rollno, name, marks) values (?,?,?)";
 
-        int rows = jdbc.update(sql, s.getRollNo(), s.getName(), s.getMarks());
+            int rows = jdbc.update(sql, s.getRollNo(), s.getName(), s.getMarks());
     
-        System.out.println(rows + " effected");
+            System.out.println(rows + " rows effected");
+        } catch (Exception e) {
+                System.out.println("0 rows effected");
+        }
+        
     }
 
     public List<Student> findAll() {
