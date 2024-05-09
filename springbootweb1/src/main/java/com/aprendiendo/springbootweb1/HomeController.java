fdc4de58f4d172a41c39f129d0aacb0428e84ce9
santiago lopez de haro <santiagolhgpro@gmail.com>
2024-05-09 17:51:35 -0300

    Se puede sumar dos números ahroa jaja

--- springbootweb1/src/main/java/com/aprendiendo/springbootweb1/HomeController.java
+++ springbootweb1/src/main/java/com/aprendiendo/springbootweb1/HomeController.java
@@ -3,6 +3,9 @@ package com.aprendiendo.springbootweb1;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class HomeController {
     
@@ -10,5 +13,18 @@ public class HomeController {
     public String home()
     {
         return "index.jsp";
+    }
+
+    @RequestMapping("/add")
+    public String add(HttpServletRequest req, HttpSession session)
+    {
+        int n1 = Integer.parseInt(req.getParameter("num1"));
+        int n2 = Integer.parseInt(req.getParameter("num2"));
+
+        int result = n1 + n2;
+        
+        session.setAttribute("resultado", result);
+
+        return "result.jsp";
     }
 }
--- springbootweb1/src/main/webapp/index.jsp
+++ springbootweb1/src/main/webapp/index.jsp
@@ -1,7 +1,17 @@
 <%@page language="java"%>
 
 <html>
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
 </body>
-    <h2>Hola Mundo !!</h2>
+    <h2>Sumar numeros</h2>
+    <form action="add">
+        <label for="num1">Enter 1st numero: </label>
+        <input type="text" id="num1" name="num1"><br>
+        <label for="num2">Enter 2nd numero: </label>
+        <input type="text" id="num2" name="num2"><br>
+        <input type="submit" value="submit">
+    </form>
 </body>
 </html>
