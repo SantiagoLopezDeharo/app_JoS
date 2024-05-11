1169a7fa30f3325e560267448f77671a2ace0839
santiago lopez de haro <santiagolhgpro@gmail.com>
2024-05-11 16:09:26 -0300

    Cree un nuevo projecto, una web basica de publicacion y vista de trabajos para poder practicar Spring junto con el curso

---
+++ jobapp/src/test/java/com/aprendiendo/jobapp/JobappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aprendiendo.jobapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
