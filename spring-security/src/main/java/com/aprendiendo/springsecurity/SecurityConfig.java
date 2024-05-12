354ccd0d646eed2141de99e0550057aec8a28b68
Santiago Lopez de haro <santiagolhgpro@gmail.com>
2024-05-12 19:03:48 -0300

    Establecimos un sistema para autenticar datos de usuarios contra una base de datos SQL en lugar de usuarios fijos en el codigo

--- spring-security/src/main/java/com/aprendiendo/springsecurity/SecurityConfig.java
+++ spring-security/src/main/java/com/aprendiendo/springsecurity/SecurityConfig.java
@@ -1,21 +1,35 @@
 package com.aprendiendo.springsecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public DaoAuthenticationProvider authProvider()
+    {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance()); // No uso encoder porque es una aplicacion de muestra
+
+        return provider;
+    }
+
     @Bean
     public SecurityFilterChain sfc(HttpSecurity hs) throws Exception
     {
@@ -30,22 +44,22 @@ public class SecurityConfig {
         return hs.build();
     } 
 
-    @Bean
-    public UserDetailsService userDetailsService()
-    {
-        UserDetails user = User
-            .withDefaultPasswordEncoder()   //Solo porque es una aplicacion de muestra
-            .username("Santiago")
-            .password("1234")
-            .roles("USER")
-            .build(); 
-        UserDetails admin = User
-            .withDefaultPasswordEncoder()   //Solo porque es una aplicacion de muestra
-            .username("admin")
-            .password("admin1234")
-            .roles("ADMIN")
-            .build();
+    // @Bean
+    // public UserDetailsService userDetailsService()
+    // {
+    //     UserDetails user = User
+    //         .withDefaultPasswordEncoder()   //Solo porque es una aplicacion de muestra
+    //         .username("Sant")
+    //         .password("1234")
+    //         .roles("USER")
+    //         .build(); 
+    //     UserDetails admin = User
+    //         .withDefaultPasswordEncoder()   //Solo porque es una aplicacion de muestra
+    //         .username("admin")
+    //         .password("admin1234")
+    //         .roles("ADMIN")
+    //         .build();
         
-        return new InMemoryUserDetailsManager(user, admin);
-    }
+    //     return new InMemoryUserDetailsManager(user, admin);
+    // }
 }
